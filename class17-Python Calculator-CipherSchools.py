09638a1e660a0ad843a44fa9660343e12c5957d9
Mohit <115339697+mohitkadiann@users.noreply.github.com>
2022-12-01 16:01:39 +0530

    Add files via upload

---
+++ class17-Python Calculator-CipherSchools.py
@@ -0,0 +1,16 @@
+
+print(5+2) #addition
+
+print(5-2) #subtraction
+
+print(5*2) #product
+
+print(5/2) #division
+
+print(5%2) #modulas..i.e. remainder
+
+print(15//4) #gives only the integer part of the quotient
+
+print(5**2) # act as power operator that is 5^2
+
+print(round(2**0.5,4))
