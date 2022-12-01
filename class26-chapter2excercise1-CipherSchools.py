2fe10f04a701e1d700d82e04c62ac75028e73cde
Mohit <115339697+mohitkadiann@users.noreply.github.com>
2022-12-01 17:00:22 +0530

    Add files via upload

---
+++ class26-chapter2excercise1-CipherSchools.py
@@ -0,0 +1,7 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+d= a+b+c
+print(f"Average is {d/3}")
+a, b, c= input("enter any three numbers separted by comma: ").split(",")
+print(f"avarage of three numbers : {(int(a)+int(b)+int(c))/3}")
