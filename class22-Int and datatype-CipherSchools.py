2d8d6212474e57289fd888ed14f65edd433f133c
Mohit <115339697+mohitkadiann@users.noreply.github.com>
2022-12-01 16:40:50 +0530

    Add files via upload

---
+++ class22-Int and datatype-CipherSchools.py
@@ -0,0 +1,14 @@
+num_one = int(input("Enter first number:")) # 2
+num_two = int(input("Enter second number:")) # 3
+sum= num_one+ num_two  # total= 23
+print("Total is "+ str(total))
+# str to make string
+# 7 --> "7"
+# int to make integer
+# "5" --> 5
+#float
+#"4" -->4.0
+num1 = str(2)
+num2= int(2)
+num3 = float(3)
+print(num2 + num3) #float
