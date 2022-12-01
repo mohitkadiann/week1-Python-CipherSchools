994357a08105c5ab3f8cabe0ad32aa8077cfc3b3
Mohit <115339697+mohitkadiann@users.noreply.github.com>
2022-12-01 16:56:04 +0530

    Add files via upload

---
+++ class25-String formatting-CipherSchools.py
@@ -0,0 +1,6 @@
+name = "Mohit"
+age = 18 
+print("hello " + "your age is "+ str(age)) # not a good looking syntax
+#string formatting
+print("hello {} your age is {} ".format(name,age+5)) #python 3
+print(f"hello {name} your age is {age+-2} ") # 3.6 neat and clean
