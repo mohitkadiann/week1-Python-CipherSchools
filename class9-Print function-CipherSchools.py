0451671833c80ec01316bf5face06f681950df4e
Mohit <115339697+mohitkadiann@users.noreply.github.com>
2022-12-01 13:59:07 +0530

    Add files via upload

---
+++ class9-Print function-CipherSchools.py
@@ -0,0 +1,6 @@
+print("Hello world")
+print('Hello world')
+print("Hello 'world' hello")
+print('Hello "world" hello')
+#can use single quotes inside double and vice versa
+#but we can not use double qoutes inside double and single quotes inside single
