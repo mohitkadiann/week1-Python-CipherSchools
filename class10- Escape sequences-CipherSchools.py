511fe14396913d3b8e791c75cc2cb543188a063c
Mohit <115339697+mohitkadiann@users.noreply.github.com>
2022-12-01 14:16:56 +0530

    Add files via upload

---
+++ class10- Escape sequences-CipherSchools.py
@@ -0,0 +1,10 @@
+# \' -> single quote
+# \"" -> double quote
+# \\ -> back slash
+# \n -> new line
+# \t -> tab
+# \b -> back space
+print("line A\nline B\nLine C")
+print("Name\tMohit")
+print("This is a double backslash\\\\")
+print("helll\bo")
