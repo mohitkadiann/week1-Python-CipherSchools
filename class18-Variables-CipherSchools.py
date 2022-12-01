7186b484b4bc8214bc112726c24525ed8232bd51
Mohit <115339697+mohitkadiann@users.noreply.github.com>
2022-12-01 16:16:16 +0530

    Add files via upload

---
+++ class18-Variables-CipherSchools.py
@@ -0,0 +1,23 @@
+num1=2 #num1 assigned 2
+print(num1)
+num1=3 #num1 assigned 3
+#python allow dynamic assignment
+
+#string
+name="Mohit"
+print(name)
+name=14189
+print(name)
+ 
+#RULE FOR NAMING OF VARIABLE
+
+# 1. Can not start with a number but number can be in between
+# 2. First letter should be a alphabet or _
+name123="MOHIT"
+ 
+#NAMING CONVENTIONS
+ 
+#1. SNAKE CASE WRITING
+# user_name_is="MOHIT"
+#2. CAMEL CASE WRITING
+userNameIs="MOHIT"
