08dbedab80cef32975c68cc95ee8be87346d6c81
Mohit <115339697+mohitkadiann@users.noreply.github.com>
2022-12-01 16:28:51 +0530

    Add files via upload

---
+++ class20-String Concatenation-CipherSchools.py
@@ -0,0 +1,9 @@
+# Addition of strings in single or double qoutes
+first_name = "Mohit"
+last_name = "Kadian"
+full_name = first_name+" " + last_name
+# print(full_name)
+# print(first_name+3) # error
+# print(first_name + str(3)) # no error
+# print(first_name + "3") # no error
+print(first_name*3)
