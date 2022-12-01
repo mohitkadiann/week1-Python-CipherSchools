ec52c577f1b1f741313e597387ebddb4f2f66c84
Mohit <115339697+mohitkadiann@users.noreply.github.com>
2022-12-01 14:36:19 +0530

    Add files via upload

---
+++ class 12-escape sequence as normal text-CipherSchools.py
@@ -0,0 +1,9 @@
+# output : Line A \n Line B
+print("Line A \\n Line B")
+print("Line B \\+ line h")
+print("this is 4 backslash \\\\\\\\")
+# output : "\'
+print(" \\\" \\\' ")
+#\' - ' (b)
+# \\ - \ (a)
+# \\\' - \'
