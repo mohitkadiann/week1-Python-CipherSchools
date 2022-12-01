f9b86dabb37131a07ea6d30f91da5d80c949db1f
Mohit <115339697+mohitkadiann@users.noreply.github.com>
2022-12-01 18:26:32 +0530

    Add files via upload

---
+++ class36-Dealing with spaces-CipherSchools.py
@@ -0,0 +1,9 @@
+name = " Moh  it   "
+dots="..........."
+# lstrip() method
+print(name+dots)
+print(name.lstrip()+dots)
+print(name.rstrip()+dots)
+print(name.strip()+dots)
+#replace method
+print(name.replace(" ", "")+ dots)
