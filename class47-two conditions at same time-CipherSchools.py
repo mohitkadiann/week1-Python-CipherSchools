bb7c262398f4b73e31b5fac2b1765fb90869979d
Mohit <115339697+mohitkadiann@users.noreply.github.com>
2022-12-01 19:45:53 +0530

    Add files via upload

---
+++ class47-two conditions at same time-CipherSchools.py
@@ -0,0 +1,13 @@
+# check two conditions at same time
+# and, or
+name='Mohit'
+age = 18
+if name=='Mohit' and age==18:
+    print("condition true")
+else:
+    print("condition false")
+
+if name == 'Mohit' or age > 19:
+    print("condition true")
+else:
+    print("condition false ")
