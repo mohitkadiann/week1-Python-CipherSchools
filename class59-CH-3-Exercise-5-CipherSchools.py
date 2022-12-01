edc31fd57bdf6f96e9f7ed9ce43acb1d2a5c1b94
Mohit <115339697+mohitkadiann@users.noreply.github.com>
2022-12-01 22:12:17 +0530

    Add files via upload

---
+++ class59-CH-3-Exercise-5-CipherSchools.py
@@ -0,0 +1,11 @@
+# solution
+
+name= input("Enter your name: ")
+#"mohit kadian"
+temp_var= ""
+i=0
+while i < len(name):
+    if name[i] not in temp_var:
+        temp_var+= name[i]
+        print(f"{name[i]}:{name.count(name[i])}")
+    i+=1
