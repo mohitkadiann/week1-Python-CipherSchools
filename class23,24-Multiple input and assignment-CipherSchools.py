df79066b91e01739a1329b32a933b11f4c5f1fb5
Mohit <115339697+mohitkadiann@users.noreply.github.com>
2022-12-01 16:49:50 +0530

    Add files via upload

---
+++ class23,24-Multiple input and assignment-CipherSchools.py
@@ -0,0 +1,9 @@
+name,age="MOHIT","24"
+print("Hello"+name+" your age is "+ age)
+x=y=z=1 #Same value to multiple variables
+print(x+y+z)
+# name= input("Enter your name: ")
+# age= input("Enter your age: ")
+name, age= input("Enter your name and age").split(",")
+print(name)
+print(age)
