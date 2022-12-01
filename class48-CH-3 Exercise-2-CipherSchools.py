095cbe7c9581b94bcf7e0b7b61ca886732498513
Mohit <115339697+mohitkadiann@users.noreply.github.com>
2022-12-01 19:54:08 +0530

    Add files via upload

---
+++ class48-CH-3 Exercise-2-CipherSchools.py
@@ -0,0 +1,9 @@
+# Exercise-WATCH COCO
+
+user_name= input("Enter your name: ")
+user_age= input("Enter your age: ")
+user_age=int(user_age)
+if user_age >=10 and (user_name[0]=='a' or user_name[0]=='A'):
+    print("You can watch coco")
+else:
+    print("you cannot watch coco")
